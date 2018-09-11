7e53b2394a8951443aee7ff1c551aa0de9da9651
Dreamwalker <aristojeff@gmail.com>
2018-09-11 13:28:40 +0900

    file path change

--- Intel Curie/treadmill_curie/unit_test/treadmill_interrupt_unit_tes/treadmill_interrupt_unit_tes.ino
+++ Intel Curie/treadmill_curie/unit_test/treadmill_interrupt_unit_tes/treadmill_interrupt_unit_tes.ino
@@ -6,15 +6,15 @@
 #include <Wire.h>
 #include <SPI.h>
 
-
-#define TREADMILL_DISTANCE            0.13F //단위 m 
-
-#define MAGNET_INTERRUPT_PIN          2
-#define BLE_LED_INDICATOR_PIN         4
-
 #define TREADMILL
 #define INDOOR_BIKE
 #define DEBUG
+
+
+#define MAGNET_INTERRUPT_PIN          2
+#define BLE_LED_INDICATOR_PIN         3
+
+#define TREADMILL_DISTANCE            0.13F //단위 m 
 
 volatile uint32_t count = 0;      // 인터럽트 클럭 카운트 수
 volatile float distance = 0.0f;   // 거리 변수 [ m]
@@ -27,6 +27,15 @@ volatile long t = 0;  // 센서 입력 외부 인터럽트 시간 변수 .
 volatile float InstantTime = 0.0f;
 volatile int diffTime = 0.0f;
 
+long saveMinTime = 0;
+
+/*************************************************************
+   인터럽트 서비스 루틴 (ISR) 함수
+   카운트 처리
+   속도 연산
+   거리 연산
+   제작 : 박제창
+ ***********************************************************/
 void interrupt_func() {
 #ifdef TREADMILL
   long interrupt_time = millis(); // 인터럽트가 발생한 시간을 저장
@@ -42,12 +51,10 @@ void interrupt_func() {
 #elif INDOORBIKE
 #else
 #endif
-
-
-
 }
 
 void setup() {
+
 #ifdef DEBUG
   Serial.begin(115200);
 #endif
@@ -62,13 +69,28 @@ void setup() {
 
 void loop() {
 
+  long currentTime = millis();
+  if (currentTime - t >= 500) {
 #ifdef DEBUG
-  Serial.print("count -> "); Serial.print(count);
-  Serial.print("| distance -> "); Serial.print(distance);
-  Serial.print("| distanceUnitKm -> "); Serial.print(distanceUnitKm);
-  Serial.print("| diffTime -> "); Serial.print(diffTime);
-  Serial.print("| InstantTime -> "); Serial.print(InstantTime);
-  Serial.print("| speedNow -> "); Serial.println(speedNow);
+    Serial.println("운동종료");
+    count = 0;
 #endif
+  } else {
+#ifdef DEBUG
+    Serial.print("count -> "); Serial.print(count);
+    Serial.print("| distance -> "); Serial.print(distance);
+    Serial.print("| distanceUnitKm -> "); Serial.print(distanceUnitKm);
+    Serial.print("| diffTime -> "); Serial.print(diffTime);
+    Serial.print("| InstantTime -> "); Serial.print(InstantTime);
+    Serial.print("| speedNow -> "); Serial.println(speedNow);
+#endif
+  }
+
+  if (currentTime - saveMinTime >= 60000) {
+    //    detachInterrupt(MAGNET_INTERRUPT_PIN);
+    //    saveMinTime = 0;
+  } else {
+
+  }
 
 }
